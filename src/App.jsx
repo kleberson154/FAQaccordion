2f4c9dce91bcf3e538fda256bd361750301c927c
kleberson154 <kleberson55@hotmail.com>
2023-12-30 15:55:34 -0300

    Style of the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Faq from './components/Faq'
+import imgBackground from './assets/images/background-pattern-desktop.svg'
 
 function App() {
   return (
     <>
-      <Faq />
+      <div className="body">
+        <img
+          className="imgBackground"
+          src={imgBackground}
+          alt="background image"
+        />
+        <Faq />
+      </div>
     </>
   )
 }
